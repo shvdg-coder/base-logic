8002127c3a05783692705a44837aa0c57c8f0906
StanHvdG <stanhvdg@gmail.com>
2024-06-09 11:42:30 +0200

    Added env value as boolean

--- pkg/environment.go
+++ pkg/environment.go
@@ -2,16 +2,27 @@ package pkg
 
 import (
 	"encoding/base64"
+	"fmt"
 	_ "github.com/joho/godotenv/autoload" // Load environment variables from a .env file
 	"os"
+	"strconv"
 )
 
-// GetValueAsString retrieves an environment string value given a key.
-func GetValueAsString(key string) string {
+// GetEnvValueAsString retrieves an environment string value given a key.
+func GetEnvValueAsString(key string) string {
 	return os.Getenv(key)
 }
 
-// GetValueAsBytes retrieves an environment []byte value given a key.
-func GetValueAsBytes(key string) ([]byte, error) {
-	return base64.StdEncoding.DecodeString(GetValueAsString(key))
+// GetEnvValueAsBoolean retrieves an environment boolean value given a key.
+func GetEnvValueAsBoolean(key string) bool {
+	boolValue, err := strconv.ParseBool(os.Getenv(key))
+	if err != nil {
+		fmt.Printf("Error while converting environment value '%s' to bool", key)
+	}
+	return boolValue
+}
+
+// GetEnvValueAsBytes retrieves an environment []byte value given a key.
+func GetEnvValueAsBytes(key string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(GetEnvValueAsString(key))
 }
